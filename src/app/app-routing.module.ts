cb718e36755d558681da730a52f4aed95f894063
issakha <diouf8987@gmail.com>
2023-06-15 17:57:31 +0100

    refactor: structuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { SecretaireComponent } from './secretaire/secretaire.component';
+import { GestionnaireComponent } from './gestionnaire/gestionnaire.component';
+import { AdminComponent } from './admin/admin.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/acceuil', pathMatch: 'full' },
+  {path:'acceuil', component:AccueilComponent},
+  {path:'patient', component:PatientComponent},
+  {path:'doctor', component:DoctorComponent},
+  {path:'secretaire', component:SecretaireComponent},
+  {path:'gestionnaire', component:GestionnaireComponent},
+  {path:'admin', component:AdminComponent}
+];
  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { PatientComponent } from './patient/patient.component';
+import { SecretaireComponent } from './secretaire/secretaire.component';
+import { GestionnaireComponent } from './gestionnaire/gestionnaire.component';
+import { AdminComponent } from './admin/admin.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorComponent,
+    PatientComponent,
+    SecretaireComponent,
+    GestionnaireComponent,
+    AdminComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule, 
